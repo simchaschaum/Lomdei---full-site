442a45db64e8165e361a2c479cd45655ee9e6e31
Simcha Schaum <simcha.schaum@gmail.com>
2021-12-22 11:31:31 +0200

    created news page and improved gallery

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,21 +1,171 @@
 const pictures = [
+    // pictures 1-35 - HALB Visit, Dec 13th, 2021
     {
-        url: '../pics/headshots/Simcha Schaum Headshot.jpg',
-        alt: "Simcha Schaum's Headshot",
-        caption: "Simcha Schaum made this website!"
+        url: '../pics/HALB Visit/IMG_0591.jpg',
+        alt: "HALB Visit",
     },
     {
-        url: '../pics/headshots/Raphael Alter Headshot.jpeg',
-        alt: "Raphael's Headshot",
-        caption: "This guy runs the show."
+        url: '../pics/HALB Visit/IMG_0594.jpg',
+        alt: "HALB Visit",
     },
     {
-        url: '../pics/lomdei classroom pic.jpg',
-        alt: "Lomdei Classroom in Action"
+        url: '../pics/HALB Visit/IMG_0595.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0596.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0597.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0598.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0599.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0600.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0601.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0603.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0604.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0605.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0606.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0607.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0608.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0609.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0610.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0611.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0612.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0613.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0614.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0615.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0616.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0617.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0618.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0619.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0620.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0621.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0624.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0625.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0626.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0628.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0629.jpg',
+        alt: "HALB Visit",
+    },
+    {
+        url: '../pics/HALB Visit/IMG_0630.jpg',
+        alt: "HALB Visit",
+    },
+    // Dec 7th visit: pics 36-42
+    {
+        url: '../pics/Dec 7 Visit/HALBVisit12_7_21 (1).jpeg',
+        alt: "Lomdei Visits HALB on December 7th"
+    },
+    {
+        url: '../pics/Dec 7 Visit/HALBVisit12_7_21 (2).jpeg',
+        alt: "Lomdei Visits HALB on December 7th"
+    },
+    {
+        url: '../pics/Dec 7 Visit/HALBVisit12_7_21 (3).jpeg',
+        alt: "Lomdei Visits HALB on December 7th"
+    },
+    {
+        url:'../pics/Dec 7 Visit/HALBVisit12_7_21 (4).jpeg',
+        alt: "Lomdei Visits HALB on December 7th"
+    },
+    {
+        url: '../pics/Dec 7 Visit/HALBVisit12_7_21 (5).jpeg',
+        alt: "Lomdei Visits HALB on December 7th"
+    },
+    {
+        url: '../pics/Dec 7 Visit/HALBVisit12_7_21 (7).jpeg',
+        alt: "Lomdei Visits HALB on December 7th"
     }
 ]
 
-const gallery = document.querySelector('#gallery-container');
+// Galleries:
+const halbGallery = document.querySelector('#halb-container');
+const dec7Gallery = document.querySelector('#dec7-container');
 const lightbox = document.querySelector('#lightbox');
 const lightboxPic = document.querySelector('#lightboxPic');
 const lightboxCap = document.querySelector('#modal-caption');
@@ -28,7 +178,11 @@ function makeGallery(){
         pic.setAttribute("src",pictures[index].url);
         pic.setAttribute("alt",pictures[index].alt);
         pic.setAttribute("onclick",`openLightbox(${index})`)
-        gallery.appendChild(pic);
+        if(index < 35){
+            halbGallery.appendChild(pic);
+        } else if(index < 42){
+            dec7Gallery.appendChild(pic);
+        }
     })
 }
 makeGallery();
